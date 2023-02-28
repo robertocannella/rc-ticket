02f8f9f29b5c13a2ee8367e83024a1232fd520c4
robertocannella <robertocannella@gmail.com>
2023-02-28 14:25:17 -0500

    Add MainController

---
+++ src/Controller/MainController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MainController extends AbstractController
+{
+    #[Route('/main', name: 'app_main')]
+    public function index(): Response
+    {
+        return $this->render('main/index.html.twig', [
+            'controller_name' => 'MainController',
+        ]);
+    }
+
+    #[Route('/custom',name:'custom')]
+    public function custom(Request $request):Response{
+        $name = $request->get('name');
+
+
+        return $this->render('main/custom.html.twig',[
+            'name' => $name
+        ]);
+    }
+}
